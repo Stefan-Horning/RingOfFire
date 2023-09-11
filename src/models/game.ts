5af4150ab7d53d9f95cd15b3ec525548d71e633d
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-09-11 22:08:01 +0200

    card Animation

---
+++ src/models/game.ts
@@ -0,0 +1,37 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playerCard: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor(){
+        for(let i = 1; i < 14; i++){
+            this.stack.push('spade_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+        shuffle(this.stack);
+    }
+
+}
+
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
